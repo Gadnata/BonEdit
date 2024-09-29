41628f029fa38dc222285aefb9e1fcc1c6c71a15
Gadnata <kovalenokgerman@gmail.com>
2024-09-29 19:00:00 +0300

    commit

--- BonEdit/urls.py
+++ BonEdit/urls.py
@@ -16,11 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-# ... the rest of your URLconf goes here ...
-
-urlpatterns += staticfiles_urlpatterns()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
