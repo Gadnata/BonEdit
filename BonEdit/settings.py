c8e94a3ca25d41c0acf4bb4c3117f5da96c120ff
Gadnata <kovalenokgerman@gmail.com>
2024-09-29 13:20:46 +0300

    commit

--- BonEdit/settings.py
+++ BonEdit/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j-3&6^x38tilx=2jokn6a5re942po0gh8iam#5gz7&&ubou+ya'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [bonedit.onrender.com localhost]
 
 
 # Application definition
