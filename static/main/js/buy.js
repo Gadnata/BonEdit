97abffc7eecf413f9a17429e6af1b00a94049916
Gadnata <kovalenokgerman@gmail.com>
2024-10-20 22:58:08 +0300

    commit

--- static/main/js/buy.js
+++ static/main/js/buy.js
@@ -1,8 +1,8 @@
 const send = () => {
   const url = 'https://api.cryptocloud.plus/v2/invoice/create';
   const headers = new Headers({
-    'Authorization': 'Token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1dWlkIjoiTWpZNE56WT0iLCJ0eXBlIjoicHJvamVjdCIsInYiOiIyODM1YjUzZjdiNWU2MTI1MDQxYmViOGRhOTk0Y2FmY2IyOWE2N2ViMWQxYWI4YTlmZTJkMThiYzRhZGQ4ZDhjIiwiZXhwIjo4ODEyOTM2NDE0Nn0.dlKn8yrbEAla42CHRvUvZcanZQC_q8QhcBM7IuUj4mY',
-    'Content-Type': 'application/json'
+      'Authorization': 'Token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1dWlkIjoiTWpZNE56WT0iLCJ0eXBlIjoicHJvamVjdCIsInYiOiIyODM1YjUzZjdiNWU2MTI1MDQxYmViOGRhOTk0Y2FmY2IyOWE2N2ViMWQxYWI4YTlmZTJkMThiYzRhZGQ4ZDhjIiwiZXhwIjo4ODEyOTM2NDE0Nn0.dlKn8yrbEAla42CHRvUvZcanZQC_q8QhcBM7IuUj4mY',
+      'Content-Type': 'application/json'
   });
   const data = {
     amount: 1,
