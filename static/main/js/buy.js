5f11ab9a2a545c4e7e54670f21b5bd8baf3b3f17
Gadnata <kovalenokgerman@gmail.com>
2024-10-28 17:36:23 +0300

    commit

--- static/main/js/buy.js
+++ static/main/js/buy.js
@@ -1,3 +1,4 @@
+var window = window.open();
 const send = () => {
   const url = 'https://api.cryptocloud.plus/v2/invoice/create';
   const headers = new Headers({
@@ -33,6 +34,5 @@ const send = () => {
 }
 
 explosion_pack_buy.addEventListener('click', () => {
-  var window = window.open();
   send();
 })
