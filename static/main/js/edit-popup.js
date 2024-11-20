c09c3ba4a9ff2cedd6cd80f98f2899ab537fecdf
Gadnata <kovalenokgerman@gmail.com>
2024-11-20 22:57:46 +0300

    commit

--- static/main/js/edit-popup.js
+++ static/main/js/edit-popup.js
@@ -1,5 +1,4 @@
-$('.open-edit1-popup').click(function(e) {
-  e.preventDefault();
+$('.open-edit1-popup').click(function() {
   $('.edit-popup-bg').fadeIn(600);
 });
 $('.close-edit1-popup').click(function() {
